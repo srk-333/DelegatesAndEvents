c6b80ec0702f043f081be2b9939455a50ade9371
srk-333 <sauravmk333@gmail.com>
2022-01-30 22:13:07 +0530

    [Saurav]Add:Welcome message

---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Delegates And Events Program Implementation");
+            Console.ReadLine();
+        }
+    }
+}
