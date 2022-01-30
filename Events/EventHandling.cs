b9081720fbc1aa7287b6df13415291fd1e6ceee6
srk-333 <sauravmk333@gmail.com>
2022-01-31 00:09:19 +0530

    [Saurav]Add:Program to Handle Events

---
+++ Events/EventHandling.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public delegate void EventHandler(string x);
+    class EventHandling
+    {
+        public class Opearation
+        {
+            public event EventHandler xyz;
+            public void Action(string a)
+            {
+                if (xyz != null)
+                {
+                    xyz(a);
+                    Console.WriteLine(a);
+                }
+                else
+                {
+                    Console.WriteLine("Not Registered");
+                }
+            }
+        }
+        private static void CatchEvent(string s)
+        {
+            Console.WriteLine("Method Calling");
+        }
+        public static void ImplementEvent()
+        {
+            Opearation op = new Opearation();
+            op.Action("Method Calling");
+            Console.WriteLine();
+            op.xyz += new EventHandler(CatchEvent);
+            op.Action("Event Called!");
+        }
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public delegate void DelEventHandler();
+    class Program
+    {
+        public static event DelEventHandler add;
+        static void Main(string[] args)
+        {
+            add += new DelEventHandler(India);
+            add += new DelEventHandler(Bhutan);
+            add += new DelEventHandler(Nepal);
+
+            add.Invoke();
+            Console.WriteLine("<----Event Handling---->");
+            EventHandling.ImplementEvent();
+            Console.ReadLine();
+        }
+        private static void India()
+        {
+            Console.WriteLine("India");
+        }
+        private static void Bhutan()
+        {
+            Console.WriteLine("Bhutan");
+        }
+        private static void Nepal()
+        {
+            Console.WriteLine("Nepal");
+        }
+    }
+}
